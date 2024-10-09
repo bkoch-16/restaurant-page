10f3fa25bcc2877850e7885fe2e30b17f5be0d96
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2024-10-08 22:56:29 -0700

    connected buttons to functions
    connected imported functions to buttons to setup tabs

--- src/index.js
+++ src/index.js
@@ -3,3 +3,29 @@ import loadHome from "./load-home.js"
 import loadMenu from "./load-menu.js"
 import loadAbout from "./load-about.js"
 
+const tabContent = document.querySelector("#content")
+
+const homeButton = document.querySelector("#home")
+const menuButton = document.querySelector("#menu")
+const aboutButton = document.querySelector("#about")
+
+homeButton.addEventListener("click", () => {
+    tabContent.replaceChildren()
+    const homeContent = loadHome()
+    
+    tabContent.appendChild(homeContent)
+})
+
+menuButton.addEventListener("click", () => {
+    tabContent.replaceChildren()
+    const menuContent = loadMenu()
+    
+    tabContent.appendChild(menuContent)
+})
+
+aboutButton.addEventListener("click", () => {
+    tabContent.replaceChildren()
+    const aboutContent = loadAbout()
+    
+    tabContent.appendChild(aboutContent)
+})
